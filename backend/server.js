7e26a086a67857fe8a9d47ec6686dd5ce98922c2
yashraj-a11y <rajy18594@gmail.com>
2025-12-05 14:27:59 +0530

    server fix

--- backend/server.js
+++ backend/server.js
@@ -17,39 +17,39 @@ const adminRoutes = require('./routes/adminRoutes')
 const productAdminRoutes = require('./routes/productAdminRoutes')
 const adminOrderRoutes = require('./routes/adminOrderRoutes')
 
-  
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 dotenv.config()
 
-const PORT= process.env.PORT || 3000 
-
+const PORT = process.env.PORT || 3000
+console.log(PORT)
 
 // connect to databse
 connectDB()
 
 
-app.get('/' , (req,res) => {
+app.get('/', (req, res) => {
     res.send('welcome to Rabbit Api')
 
 })
 
 // API Routes 
-app.use('/api/users',userRoutes)
-app.use('/api/products' , productRoutes)
-app.use('/api/cart' , cartRoutes)
-app.use('/api/checkout' , checkoutRoutes)
-app.use('/api/orders' ,orderRoutes)
-app.use('/api/upload' ,uploadRoutes)
-app.use('/api/subscribe' ,subscribeRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/cart', cartRoutes)
+app.use('/api/checkout', checkoutRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+app.use('/api/subscribe', subscribeRoutes)
 
 
 // /Admin 
 app.use('/api/admin/users', adminRoutes)
-app.use('/api/admin/products' , productAdminRoutes)
-app.use('/api/admin/orders' , adminOrderRoutes)
+app.use('/api/admin/products', productAdminRoutes)
+app.use('/api/admin/orders', adminOrderRoutes)
 
 
 
@@ -58,5 +58,4 @@ app.use('/api/admin/orders' , adminOrderRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
-    
 })
